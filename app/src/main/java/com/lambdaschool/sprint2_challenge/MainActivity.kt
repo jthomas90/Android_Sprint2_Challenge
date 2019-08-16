40d5d48d0b237d341fcdbe4683bbc2dfb7aa48f7
Janiece Thomas <janiece.t90@gmail.com>
2019-08-16 17:01:36 -0400

    Created created intent that should trigger the share (not finished yet) working on notification as well at same time

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
@@ -1,4 +1,8 @@
 package com.lambdaschool.sprint2_challenge
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,8 +23,37 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        //triggering notification and intent taking orderList and taking it to Messaging app
+        share_button.setOnClickListener {
+           val shareIntent: Intent = Intent().apply {
+               action = Intent.ACTION_SEND
+               putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+               type = "text/plain"
+           }
+            startActivity(shareIntent)
+
+
+
+        }
+
 
 
 
     }
+
+    fun notificationSaver(orderList: String) {
+        val channelID = "${this.packageName}.simplechannel"
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        }
+    }
+
+
+
+
+
+
+
 }
