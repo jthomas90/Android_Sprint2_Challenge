399f34cdc3526a3e4410e3f46821cc4fb06dfab3
Janiece Thomas <janiece.t90@gmail.com>
2019-08-16 12:07:06 -0400

    Created Groceryrepo completely

--- app/src/main/java/com/lambdaschool/sprint2_challenge/GrocerlistRepo.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/GrocerlistRepo.kt
@@ -1,4 +1,18 @@
 package com.lambdaschool.sprint2_challenge
 
+import com.lambdaschool.sprint2_challenge.ShoppingItemConstants.ICON_IDS
+import com.lambdaschool.sprint2_challenge.ShoppingItemConstants.ITEM_NAMES_RAW
+
 class GrocerlistRepo {
+    companion object{
+        val grocerylist = mutableListOf<ShoppingListModel>()
+        fun createGroceryList () {
+            for (i in 0 until ICON_IDS.size ){
+                grocerylist.add(ShoppingListModel(ITEM_NAMES_RAW[i], ICON_IDS[i],false ))
+
+            }
+        }
+
+    }
 }
+
