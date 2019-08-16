48bf9c12255d1aca6da2126d3afeb62f6a71d914
Janiece Thomas <janiece.t90@gmail.com>
2019-08-16 17:30:05 -0400

    zoom chat

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.lambdaschool.sprint2_challenge
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,35 +27,46 @@ class MainActivity : AppCompatActivity() {
 
         //triggering notification and intent taking orderList and taking it to Messaging app
         share_button.setOnClickListener {
-           val shareIntent: Intent = Intent().apply {
-               action = Intent.ACTION_SEND
-               putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
-               type = "text/plain"
-           }
+            saverNotification()
+            val shareIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, () )//this will hold my orderlist
+                type = "text/plain"
+            }
             startActivity(shareIntent)
 
 
-
         }
-
-
 
 
     }
 
-    fun notificationSaver(orderList: String) {
-        val channelID = "${this.packageName}.simplechannel"
+    fun saverNotification(orderList: String) {
+        val channelId = "${this.packageName}.grocerychannel"
         val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Grocery Notification Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val description = "Channel to send shopping notification"
 
+            val channel = NotificationChannel(channelId, name, importance)
+            channel.description = description
+
+            notificationManager.createNotificationChannel(channel)
         }
+
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .setSmallIcon(android.R.drawable.ic_dialog_alert)
+            .setContentTitle("Confimation")
+            .setContentText("Order Has Been Placed")
+            .setAutoCancel(true)
+        notificationManager.notify(10, notificationBuilder.build())
+
+
+
     }
-
-
-
-
-
-
 
 }
